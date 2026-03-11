f000fbefd7097117a7afedff80f63749bda230ce
dmitriginzburg <gremlin272@gmail.com>
2026-03-11 13:11:17 +0200

    header is changed

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -13,31 +13,35 @@ const AboutMe = () => {
     });
 
     useEffect(() => {
+
         if (!(heroId in characters)) {
             return;
         }
-        if (!hero) {
-            fetch(`${characters[heroId as keyof typeof characters].url}`)
-                .then(response => response.json())
-                .then(data => {
-                    const info = {
-                        name: data.name,
-                        gender: data.gender,
-                        birth_year: data.birth_year,
-                        height: data.height,
-                        mass: data.mass,
-                        hair_color: data.hair_color,
-                        skin_color: data.skin_color,
-                        eye_color: data.eye_color
-                    }
-                    setHero(info);
-                    localStorage.setItem(heroId, JSON.stringify({
-                        payload: info,
-                        timestamp: Date.now()
-                    }));
-                })
-        }
-    }, [heroId])
+
+        fetch(`${characters[heroId as keyof typeof characters].url}`)
+            .then(response => response.json())
+            .then(data => {
+
+                const info = {
+                    name: data.name,
+                    gender: data.gender,
+                    birth_year: data.birth_year,
+                    height: data.height,
+                    mass: data.mass,
+                    hair_color: data.hair_color,
+                    skin_color: data.skin_color,
+                    eye_color: data.eye_color
+                }
+
+                setHero(info);
+
+                localStorage.setItem(heroId, JSON.stringify({
+                    payload: info,
+                    timestamp: Date.now()
+                }));
+            });
+
+    }, [heroId]);
 
     return (heroId in characters) ? (
         <>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,30 @@
 import Navigation from "./Navigation.tsx";
+import {useLocation} from "react-router";
+import {characters, defaultHero, navItems} from "../utils/constants.ts";
 
 const Header = () => {
+
+    const {pathname} = useLocation();
+    const parts = pathname.split("/");
+
+    const isAboutPage =
+        parts[1]?.toLowerCase() === navItems[1].toLowerCase().replace(" ", "%20");
+
+    const heroId = parts[2] ? parts[2] : defaultHero;
+
+    const heroName =
+        characters[heroId as keyof typeof characters]?.name || "Luke Skywalker";
+
     return (
         <header className="rounded-t-3xl bg-gray">
             <Navigation/>
-            <h1 className="text-center text-4xl py-6">Luke Skywalker</h1>
+
+            {isAboutPage && (
+                <h1 className="text-center text-4xl py-6">
+                    {heroName}
+                </h1>
+            )}
+
         </header>
     )
 }
